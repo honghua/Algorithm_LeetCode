b7d30234098156f20d760f04a52d49872ca1b417
honghua <yanghh7@gmail.com>
2019-11-08 14:14:04 -0800

    BFS mark visited at generation

--- bfs/IsBipartite.java
+++ bfs/IsBipartite.java
@@ -12,12 +12,13 @@ public class IsBipartite {
         Map<Integer, Integer> color = new HashMap<>();
         for (int i = 0; i < graph.length; i++) {
             if (!color.containsKey(i) && !isBipartite(i, color, graph)) return false;
+            // not visited  && Not a Bipartite ==> return false
         }
         return true;
     }
     private boolean isBipartite(Integer node, Map<Integer, Integer> color, int[][] graph) {
         Queue<Integer> queue = new ArrayDeque<>();
-        color.put(node, -1);
+        color.put(node, -1); // mark visited at generation
         queue.offer(node);
         
         while (!queue.isEmpty()) {
@@ -27,7 +28,7 @@ public class IsBipartite {
             for (int nei : graph[cur]) {
                 int neiColor = color.getOrDefault(nei, 0);
                 if (neiColor == 0) {
-                    color.put(nei, -curColor);
+                    color.put(nei, -curColor); // mark visited at generation
                     queue.offer(nei);
                 } else if (neiColor == curColor) {
                     return false;
